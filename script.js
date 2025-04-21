14c6b3e56067f5b11e2dad55bcd80885acf4e5b7
Vishwa R <vishwapgp31@gmail.com>
2025-04-21 21:24:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ localStorage.setItem('score',JSON.stringify(score));
 updateScoresElement();
 
 document.querySelector('.js-result').innerHTML =result;
-document.querySelector('.js-moves').innerHTML =`You<img src="images/${playerMove}-emoji.png" class="move-icon"><img src="images/${computerMove}-emoji.png" class="move-icon">Computer.`;
+document.querySelector('.js-moves').innerHTML =`You<img src="${playerMove}-emoji.png" class="move-icon"><img src="${computerMove}-emoji.png" class="move-icon">Computer.`;
 }
 //update scores
 function updateScoresElement()
